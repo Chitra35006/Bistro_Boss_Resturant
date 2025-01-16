42ec7f51eef94c2ba4557b6a633e51196fe0b2ea
Chitra35006 <ch18.cse@gmail.com>
2025-01-16 15:07:39 +0600

    role & give access different for a admin & user

--- src/Providers/AuthProvider.jsx
+++ src/Providers/AuthProvider.jsx
@@ -2,6 +2,7 @@ import { createContext, useEffect, useState } from "react";
 
 import { createUserWithEmailAndPassword, getAuth, GoogleAuthProvider, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from "firebase/auth";
 import { app } from "../components/Firebase/firebase.config";
+import useAxiosPublic from "../hooks/useAxiosPublic";
 
 
 
@@ -9,6 +10,7 @@ export const AuthContext = createContext(null);
 
 // Initialize Firebase Authentication and get a reference to the service
 const auth = getAuth(app);
+const axiosPublic = useAxiosPublic();
 
 const AuthProvider = ({children}) => {
 
@@ -52,13 +54,28 @@ const AuthProvider = ({children}) => {
     useEffect(() => {
         const unsubscribe = onAuthStateChanged(auth, currentUser => {
             setUser(currentUser);
-            console.log('current user', currentUser);
+            // console.log('current user', currentUser);
+            if(currentUser){
+                //get token and store client
+                const userInfo = { email: currentUser.email };
+                axiosPublic.post('/jwt', userInfo)
+                    .then(res => {
+                        if (res.data.token) {
+                            localStorage.setItem('access-token', res.data.token);
+                        }
+                    })
+            }
+            else{
+               //TODO: remove token (if token stored in the client side--->>> Local storage, caching, in memory)
+               localStorage.removeItem('access-token');
+
+            }
             setLoading(false);
         });
         return () => {
             return unsubscribe();
         }
-    }, [])
+    }, [axiosPublic])
 
 
     const authInfo = {
--- src/components/Pages/DashBoard/Allusers.jsx
+++ src/components/Pages/DashBoard/Allusers.jsx
@@ -9,7 +9,10 @@ const Allusers = () => {
     const { data: users = [], refetch } = useQuery({
         queryKey: ['users'],
         queryFn: async () => {
-            const res = await axiosSecure.get('/users');
+        const res = await axiosSecure.get('/users');
+
+           
+
             return res.data;
         }
     })
